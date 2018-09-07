0a396b9fb7eb3794dd8893960865493f92b8cd62
Adam Blinkinsop <blinks@acm.org>
2018-09-07 14:22:54 -0700

    Simplified.

--- src/sprue/core.clj
+++ src/sprue/core.clj
@@ -1,9 +1,10 @@
 (ns sprue.core
   "Ties together card data, card templates, and sheet templates."
-  (:import java.io.StringReader
-           org.apache.batik.transcoder.image.PNGTranscoder)
-  (:require [clojure.xml :as xml])
-  (:use sprue.card sprue.sheet))
+  (:import [java.io StringReader]
+           [org.apache.batik.transcoder TranscoderInput TranscoderOutput]
+           [org.apache.batik.transcoder.image PNGTranscoder])
+  (:require [clojure.xml :as xml]
+            [clojure.java.io :as io]))
 
 ; Three points of user interaction:
 ; 1. Card data, slurped in from a record-oriented data file.
@@ -14,20 +15,32 @@
 
 ; 2. Card layout, as SVG and perhaps CSS.
 ;    Anything that can generate a valid SVG document.
+(defn poker-card ; TODO(#3): implement.
+  [& content]
+  {:tag :svg
+   :attrs {:xmlns "http://www.w3.org/2000/svg" :width "2.5in" :height "3.5in"}
+   :content content})
 
 ; 3. Sheet layout, as SVG and Batik Transcoder options.
 ;    https://xmlgraphics.apache.org/batik/using/transcoder.html
 ; TODO
+(defn svg->string [svg] (with-out-str (xml/emit svg)))
+(defn svg->png
+  [svg png]
+  (with-open [reader (StringReader. (svg->string svg))
+              writer (io/output-stream png)] ; PNG doesn't support writer.
+    (let [tr (PNGTranscoder.)
+          in (TranscoderInput. reader)
+          out (TranscoderOutput. writer)]
+      (.transcode tr in out)
+      (.flush writer))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [t (PNGTranscoder.)]
-    (transcode t
-               (poker-card {:tag :rect
-                            :attrs {:x "1in"
-                                    :y "1in"
-                                    :width "1in"
-                                    :height "1in"
-                                    :style "fill: black;"}})
-               "output.png")))
+  (svg->png
+    (poker-card {:tag :text
+                 :attrs {:x "1in" :y "1in" :fill "black"}
+                 :content ["Hello, world!"]})
+    "output.png"))
